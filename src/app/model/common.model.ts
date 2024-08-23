2b8572a73ed08c1ba81319aa7c945289ffa064ac
Kittisakk <kittisakk@wisesoft.co.th>
2024-08-23 21:33:57 +0700

    [fix] req field

--- src/app/model/common.model.ts
+++ src/app/model/common.model.ts
@@ -1,5 +1,5 @@
 export interface CraftspersonModel {
-  craftsperson_id?: number;  // generated field, so it is optional
+  craftsperson_id: number;  // generated field, so it is optional
   craftsperson_name: string;
   address?: string;
   contact?: string;
